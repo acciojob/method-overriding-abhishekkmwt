08b18af97a5c0c51638a5edaa241554411b4b50b
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-03-14 14:56:33 +0530

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        B obj=new B();
+        String ans1=obj.meth();
+        String ans2=obj.meth();
+        System.out.println(ans1+"    "+ans2);
+    }
   
 }
